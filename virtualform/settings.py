e2a03582a833926ea187c2d0d36379fa1f6e259f
yanap <tatiyanavoron19@gmail.com>
2023-08-03 02:29:10 +0600

    initial commit

--- virtualform/settings.py
+++ virtualform/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-js@_a&&jbd*!gt-ogdx5a^!$eyhdc_lf96+g#(qy^6=$p%waq(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
